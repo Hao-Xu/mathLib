d772ad5b0b5df6dac524c28d62c097358d31bef5
Hao Xu <haoxu@lbl.gov>
2016-09-14 10:55:15 -0700

    Initialization

---
+++ typedefs.h
@@ -0,0 +1,18 @@
+// typedefs.h
+//   Types defined for math library.
+// History:
+// 2016/09/13  Hao Xu   First release
+
+#ifndef TYPEDEFS_H
+#define TYPEDEFS_H
+
+#include<vector>
+
+using namespace std;
+
+typedef vector<vector<double> > r2Tensor;
+
+
+
+
+#endif /* TYPEDEFS_H */
